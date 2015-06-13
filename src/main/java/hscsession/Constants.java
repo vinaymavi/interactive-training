9ec72dfca8cc2d4471d95ab9d9331669119b7e56
vinay mavi <vinaymavi@gmail.com>
2015-06-13 14:06:38 +0530

    appengine running application

---
+++ src/main/java/hscsession/Constants.java
@@ -0,0 +1,13 @@
+package hscsession;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming your API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "162865267361-m40cis4kiqhq8dt78kufp9j4sddsaqm9.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
